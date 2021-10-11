01a81bc8cae232f0cdf81341f3d2c36edac0dd1d
Kimochii-x3 <38957871+Kimochii-x3@users.noreply.github.com>
2021-10-11 21:50:36 +0100

    .

--- flmain.js
+++ flmain.js
@@ -167,7 +167,8 @@ bot.on('messageCreate', async message => {
             if (!message.content.toLowerCase().startsWith(prefix)) { return; }
             const args = message.content.slice(prefix.length).split(/ +/);
             const cmdName = args.shift().toLowerCase();
-            const option = message.content.slice(prefix.length + cmdName.length).split(/-+/).then(entry => entry.trim());
+            const option0 = message.content.slice(prefix.length + cmdName.length).split(/-+/);
+            const option = option0.map(entry => entry.trim());
             // await option.map(entry => {return entry.trim()});
             if (!commands.has(cmdName)) { return; }
             const cmd = commands.get(cmdName);
