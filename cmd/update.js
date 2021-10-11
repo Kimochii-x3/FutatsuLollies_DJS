e25ce7b4df15f4778876a25f2ce38fa3eea17aa5
Kimochii-x3 <38957871+Kimochii-x3@users.noreply.github.com>
2021-10-11 21:01:26 +0100

    add of update command to pull the code from github

--- cmd/update.js
+++ cmd/update.js
@@ -8,13 +8,19 @@ module.exports = {
     guildOnly: false,
     args: false,
     async execute(bot, message, args, option, commands, prefix) {
+        // prefix and command length get removed, option[0] is always the text before the first > - < (minus).
         if (message.author.id === (bot.owner.id || bot.maintainer.id)) {
+            console.log(`test? ${option}`);
             if (option[1] === 'set' && option[2] === 'motd') {
+                console.log(`${option[0]} + ${option[1]}`);
                 await bot.db.query('update botStats set motd = ?', [option[0]]).catch(bot.errHandle);
                 await bot.user.setActivity(`${bot.guilds.cache.size} servers / MOTD: ${option[0]}`, { type: 'watching' }).catch(bot.errHandle);
             } else if (option[1] === 'delete' && option[2] === 'motd') {
+                console.log(`${option[0]} + ${option[1]}`);
                 await bot.db.query(`update botStats set motd =NULL`).catch(bot.errHandle);
                 await bot.user.setActivity(`${bot.guilds.cache.size} servers`, { type: 'watching' }).catch(bot.errHandle);
+            } else if (option[1] === 'update') {
+                require("child_process").execSync("git pull").then((out) => message.channel.send(out, {code: "css"}).catch((err) => message.channel.send(err, {code: "js"})));
             }
         } else if (message.author.id !== (bot.owner.id || bot.maintainer.id)) {
             return message.react('🤔').catch(bot.errHandle);;
