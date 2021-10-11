6a4444a94da06a28b4a381b4dd0b861691e2607a
Kimochii-x3 <38957871+Kimochii-x3@users.noreply.github.com>
2021-10-11 21:07:57 +0100

    .

--- cmd/update.js
+++ cmd/update.js
@@ -10,13 +10,15 @@ module.exports = {
     async execute(bot, message, args, option, commands, prefix) {
         // prefix and command length get removed, option[0] is always the text before the first > - < (minus).
         if (message.author.id === (bot.owner.id || bot.maintainer.id)) {
-            console.log(`test? ${option}`);
+            console.log(`console log option array: ${option}`);
+            console.log(`console log of option 0, 1 and 2 separated: ${option[0]} + ${option[1]} + ${option[2]}`);
+            console.log(`console log args: ${args}`);
             if (option[1] === 'set' && option[2] === 'motd') {
-                console.log(`${option[0]} + ${option[1]}`);
+                console.log(`console log setting motd: ${option[1]} + ${option[2]}`);
                 await bot.db.query('update botStats set motd = ?', [option[0]]).catch(bot.errHandle);
                 await bot.user.setActivity(`${bot.guilds.cache.size} servers / MOTD: ${option[0]}`, { type: 'watching' }).catch(bot.errHandle);
             } else if (option[1] === 'delete' && option[2] === 'motd') {
-                console.log(`${option[0]} + ${option[1]}`);
+                console.log(`console log deleting motd: ${option[1]} + ${option[2]}`);
                 await bot.db.query(`update botStats set motd =NULL`).catch(bot.errHandle);
                 await bot.user.setActivity(`${bot.guilds.cache.size} servers`, { type: 'watching' }).catch(bot.errHandle);
             } else if (option[1] === 'update') {
