fa7cbd97b35c605896713f3c0afcc8824dd0d15f
krista-chan <qbotdev84@gmail.com>
2021-10-23 16:59:37 +0100

    log links so that an embed sends for every link present

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,7 +1,7 @@
 const {MessageEmbed} = require('discord.js');
 
 module.exports = async (bot, message) => {
-    if (message.channel.type !== 'dm') {
+    if (message.channel.type !== 'dm' || message.author.bot) {
         const rows = await bot.db.query('SELECT * FROM serverInfo WHERE serverID = ?', [message.guild.id]).catch(bot.errHandle);
 		if(rows != undefined) {
 			const botPerms = message.guild.me.permissions.has(['SEND_MESSAGES', 'EMBED_LINKS'], { checkAdmin: true });
@@ -10,13 +10,27 @@ module.exports = async (bot, message) => {
 				if (rows[0].serverLog === 'Y' && logCHNL) {
 					const mFiles = message.attachments.map(a => a.proxyURL);
 					if (mFiles.length === 0) {
+						const linkRegex = /http(s?):\/\/(\w*\.)?\w+\.\w+/g;
+						const link = linkRegex.exec(message.content);
+						let imageEmbeds = [];
+
+						if (link.length > 0) {
+							for (const image of link) {
+								imageEmbeds.push(new MessageEmbed()
+								.setAuthor(message.author.tag, message.author.displayAvatarURL())
+								.setColor('#c4150f')
+								.setTimestamp()
+								.setImage(image))
+							}
+						}
+
 						const mDelete = new MessageEmbed()
 						.setAuthor(message.author.tag, message.author.displayAvatarURL())
 						.setDescription(`**Message sent by **${message.author}** was deleted in **${message.channel}\n${message.content}`)
 						.setColor('#c4150f')
 						.setTimestamp()
 						.setFooter(`Author ID: ${message.author.id} & Message ID: ${message.id}`);
-						return logCHNL.send({embeds: [mDelete]});
+						return logCHNL.send({embeds: [mDelete, ...imageEmbeds]});
 					} else {
 						mFiles.forEach(async (a) => {
 							if (['.png', '.jpg', '.gif', '.jpeg', '.tiff', '.tif', '.bmp'].some(e => a.includes(e))) {
