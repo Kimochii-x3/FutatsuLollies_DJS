9a11a912c09a8c348eb6c986e4165f62c9f2320a
krista-chan <qbotdev84@gmail.com>
2021-10-26 18:31:29 +0100

    Format eval output and limit length to stop api errors

--- cmd/eval.js
+++ cmd/eval.js
@@ -42,7 +42,7 @@ module.exports = {
                             name: "Input 📥", value: `\`\`\`js\n${args.join(" ")}\n\`\`\``
                         },
                         {
-                            name: "Output 📤", value: `\`\`\`js\n${shorten(output, 100, "output shortened")}\n\`\`\``
+                            name: "Output 📤", value: `\`\`\`js\n${shorten(output, 998, "output longer than 1024")}\n\`\`\``
                         },
                         {
                             name: "Type", value: `\`${typeof output}\``
