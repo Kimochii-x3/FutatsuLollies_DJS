2aafc322f5502aca671322d17502e0a3a09d8d49
krista-chan <qbotdev84@gmail.com>
2022-01-04 19:21:22 -0500

    finally setup test bot to upload

--- cmd/update.js
+++ cmd/update.js
@@ -116,7 +116,7 @@ module.exports = {
                                         let commit = require("child_process").execSync(`git commit -am "${msg.content}"`).toString();
                                         await botMsg.edit({embeds: [botMsg.embeds[0].setTitle('Code commited!').spliceFields(0, 0, [{name: 'Output - Commit', value: `\`\`\`CSS\n${commit}\`\`\``, inline: false}])]}).catch(bot.errHandle);
                                         await new Promise(wait => setTimeout(wait, 3000));
-                                        let uploadCode = require("child_process").execSync("upcode").toString();
+                                        let uploadCode = require("child_process").execSync("git push https://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@github.com/Kimochii-x3/FutatsuLollies_DJS.git --force").toString();
                                         await botMsg.edit({embeds: [botMsg.embeds[0].setTitle('Code uploaded!').spliceFields(0, 1, [{name: 'Output - Upload', value: `\`\`\`CSS\n${uploadCode}\`\`\``, inline: false}])]}).catch(bot.errHandle);
                                         await new Promise(wait => setTimeout(wait, 4000));
                                         collector.stop();
